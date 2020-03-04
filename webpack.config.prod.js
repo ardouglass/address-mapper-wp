68f85ec93318698b5ec011585872fe947ec6d16e
Andrew Douglass <andrew@dou.glass>
2020-03-04 13:05:29 -0600

    Added settings API endpoint.
    Also added Prettier and the Prettier PHP plugin. Ignore whitespace changes!
    
    Closes #8.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './frontend/index.js',
   output: {
     filename: 'address-mapper.min.js',
@@ -9,8 +9,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      pages: path.resolve(__dirname, '../frontend/pages/'),
-      components: path.resolve(__dirname, '../frontend/components/'),
+      pages: path.resolve(__dirname, 'frontend/pages/'),
+      components: path.resolve(__dirname, 'frontend/components/'),
     },
   },
   module: {
@@ -23,13 +23,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          { loader: 'style-loader' },
+          {loader: 'style-loader'},
           {
             loader: 'css-loader',
             options: {
               modules: {
-                localIdentName: '[name]__[local]--[hash:base64:5]',
-                context: path.resolve(__dirname, 'frontend'),
+                localIdentName: '[hash:base64:5]',
               },
             },
           },
